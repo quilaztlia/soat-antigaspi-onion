3e5d9854f3c2123065118f3cea8ff3d767a6cf97
Adriana NAVA AGUILAR <tennamiqui@hotmail.com>
2022-07-29 13:20:46 +0200

    Add Exception Handler

---
+++ src/Web/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,10 @@
+﻿namespace Antigaspi.Web.Middleware
+{
+    internal sealed class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
